d5e2a974bfdf005184c2584d42d9d329d59b30e6
mlwelker <nutria.watts.0g@icloud.com>
2023-08-09 18:02:35 -0500

    Merge remote-tracking branch 'refs/remotes/origin/main'

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -2,43 +2,141 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
+    @State private var score = 0
+    
+    @FocusState private var focus: Bool
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-    
-    func loadThrowingFile() throws {
-        if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt") {
-            let fileContents = try String(contentsOf: fileURL)
-            print(fileContents)
-        }
-        // modified
-    }
-    
-    func loadFile() {
-        if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt") {
-            if let fileContents = try? String(contentsOf: fileURL) {
-                // file is loaded into string
-                print(fileContents)
+        NavigationView {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord).textInputAutocapitalization(.never)
+                        .focused($focus)
+                }
+                Section {
+                    Text("Score: \(score)")
+                }
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
+            .onAppear(perform: startGame)
+            .alert(errorTitle, isPresented: $showingError) {
+                Button("OK", role: .cancel) { }
+            } message: {
+                Text(errorMessage)
+            }
+            .toolbar {
+                Button("New word") {
+                    startGame()
+                }
             }
         }
     }
     
-    func test() {
-        let input = """
-a
-b
-c
-"""
-        let letters = input.components(separatedBy: "\n")
-        let letter = letters.randomElement()
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        guard answer.count > 0 else { return }
         
-        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
+        // TODO extra validation
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word already used", message: "Be more original.")
+            return
+        }
+        
+        guard isPossible(word: answer) else {
+            wordError(title: "Impossible word", message: "Use only the letters available in the word: \(rootWord).")
+            return
+        }
+        
+        guard isReal(word: answer) else {
+            wordError(title: "Not a real word", message: "You can't use made-up words.")
+            return
+        }
+        
+        guard isLongEnough(word: answer) else {
+            wordError(title: "Too short", message: "Words must be 3 characters or longer.")
+            return
+        }
+        
+        guard differentFromRoot(word: answer) else {
+            wordError(title: "Same as root", message: "Word cannot be the same as the root word.")
+            return
+        }
+        
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+            score += answer.count
+        }
+        newWord = ""
+        focus = true
+    }
+    
+    func startGame() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                let allWords = startWords.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "silkworm"
+                return
+            }
+        }
+        
+        fatalError("Could not load 'start.txt'")
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func isLongEnough(word: String) -> Bool {
+        word.count >= 3
+    }
+    
+    func differentFromRoot(word: String) -> Bool {
+        word != rootWord
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
     }
 }
 
